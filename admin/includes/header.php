9aa378dc60abdf210ad01c3b0d0a32ab19718215
kyboahene <kyeiyaw437@gmail.com>
2020-06-11 11:11:31 +0000

    header

---
+++ admin/includes/header.php
@@ -0,0 +1,7 @@
+<title>VoteApp</title>
+
+<!-- Required meta tags -->
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+<link rel="icon" href="./images/ecLogo.png" sizes="19x19" />
