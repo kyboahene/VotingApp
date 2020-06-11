fea501067dbd0aa5c87dd654e0a6aa8dfd87b1dd
kyboahene <kyeiyaw437@gmail.com>
2020-06-11 11:11:21 +0000

    footer

---
+++ admin/includes/footer.php
@@ -0,0 +1,11 @@
+<!-- Footer -->
+<footer class="page-footer font-small p-2 justify-content-center d-flex" style="background-color: #043a5a;">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3" style="background-color: #043a5a; padding: 20px">&copy; <?php echo date("Y"); ?> Copyright:
+        <a href="#"> ELECTORAL COMMISSION GHANA</a>
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer --2
